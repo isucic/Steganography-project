f49b899071373197eceaeb3e28451fe36274c3ee
isucic <isucic00@fesb.hr>
2023-05-22 14:36:08 +0200

    Dodan GUI

--- steganography.py
+++ steganography.py
@@ -1,5 +1,9 @@
 import numpy as np
 import cv2
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+from tkinter import ttk
 
 # Pretvaranje podataka u binarni string
 # Za svaku vrstu podataka "data" (string, byte, niz, integer) funkcijom format() vrši se pretvorba svakog znaka u 8-bitni binarni oblik te se on povezuje s funkcijom join().
@@ -73,3 +77,44 @@ def encode(name, secret):
                 break
 
     return image
+
+
+image_display_size = 150, 150
+
+
+def on_click():
+    global path_image
+    # use the tkinter filedialog library to open the file using a dialog box.
+    # obtain the image of the path
+    path_image = filedialog.askopenfilename()
+    # load the image using the path
+    load_image = Image.open(path_image)
+    # set the image into the GUI using the thumbnail function from tkinter
+    load_image.thumbnail(image_display_size, Image.ANTIALIAS)
+    # load the image as a numpy array for efficient computation and change the type to unsigned integer
+    np_load_image = np.asarray(load_image)
+    np_load_image = Image.fromarray(np.uint8(np_load_image))
+    render = ImageTk.PhotoImage(np_load_image)
+    img = Label(app, image=render)
+    img.image = render
+    img.place(x=210, y=60)
+
+
+# GUI
+# Defined the TKinter object app with background lavender, title Encrypt, and app size 600*600 pixels.
+app = Tk()
+app.configure(background='black')
+app.title("Encrypt")
+app.geometry('600x600')
+# create a button for calling the function on_click
+on_click_button = Button(app, text="Choose Image", command=on_click)
+on_click_button.place(x=250, y=10)
+
+# add a text box using tkinter's Text function and place it at (340,55). The text box is of height 165pixels.
+txt = Text(app, wrap=WORD, width=60)
+txt.place(x=50, y=250, height=80)
+
+encrypt_button = Button(app, text="Encode", bg='white',
+                        fg='black', command=encode("image.png", "string"))
+encrypt_button.place(x=435, y=350)
+app.mainloop()
